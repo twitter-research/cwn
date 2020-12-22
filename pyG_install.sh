e917aa41dd16ac0691f7a9d4c80e3fa50e3992c3
Cristian Bodnar <crisbodnar96@gmail.com>
2020-12-22 15:57:54 +0200

    Clean requirements and add install script for pyG

---
+++ pyG_install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+TORCH=1.7.0
+CUDA=cu102
+pip install torch-scatter -f https://pytorch-geometric.com/whl/torch-${TORCH}+${CUDA}.html
+pip install torch-sparse -f https://pytorch-geometric.com/whl/torch-${TORCH}+${CUDA}.html
+pip install torch-cluster -f https://pytorch-geometric.com/whl/torch-${TORCH}+${CUDA}.html
+pip install torch-spline-conv -f https://pytorch-geometric.com/whl/torch-${TORCH}+${CUDA}.html
+pip install torch-geometric
