49378c3dba549d9a784e69fc647c284628eb49df
Cristian Bodnar <crisbodnar96@gmail.com>
2021-01-14 12:08:02 +0000

    Add TODOs and minimal test

--- mp/test_models.py
+++ mp/test_models.py
@@ -1,3 +1,4 @@
+import torch
 import pytest
 
 from mp.models import DummyChainMessagePassing, DummySimplicialMessagePassing
@@ -5,7 +6,7 @@ from mp.models import DummyChainMessagePassing, DummySimplicialMessagePassing
 
 @pytest.fixture
 def build_dummy_smp():
-    return DummySimplicialMessagePassing(
+    return lambda: DummySimplicialMessagePassing(
         DummyChainMessagePassing(),
         DummyChainMessagePassing(),
         DummyChainMessagePassing(),
@@ -13,4 +14,6 @@ def build_dummy_smp():
 
 
 def test_propagation_in_dummy_simplicial_message_passing(build_dummy_smp):
-    pass
+    # TODO(Cris): Use the data.py data structures for this test once they are ready.
+    # TODO(Cris): Create a house dataset to be reused across all test.
+    dsmp = build_dummy_smp()
