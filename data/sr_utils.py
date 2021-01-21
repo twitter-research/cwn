07a9c56fdf03646bcce72f60c71952e8c0379624
Noired <ffrasca@twitter.com>
2021-01-21 11:37:07 +0000

    Added SR-graph data and utils to load them

---
+++ data/sr_utils.py
@@ -0,0 +1,14 @@
+import networkx as nx
+import torch
+from torch_geometric.utils import to_undirected
+
+def load_sr_dataset(path):
+    
+    nx_graphs = nx.read_graph6(path)
+    graphs = list()
+    for nx_graph in nx_graphs:
+        n = nx_graph.number_of_nodes()
+        edge_index = to_undirected(torch.tensor(list(nx_graph.edges()), dtype=torch.long).transpose(1,0))
+        graphs.append((edge_index, n))
+        
+    return graphs
