26f22c4fe47c578ee29232f5d0cf66a22c3d0f1d
Cristian Bodnar <crisbodnar96@gmail.com>
2021-01-30 16:31:14 +0000

    Uncomment test

--- mp/test_models.py
+++ mp/test_models.py
@@ -42,38 +42,38 @@ def test_sin_model_with_batching():
         assert torch.allclose(preds, batched_preds)
 
 
-# def test_sin_model_with_batching_over_complexes_missing_triangles():
-#     """Check this runs without errors"""
-#     data_list = get_complex_list()
-#     data_loader = DataLoader(data_list, batch_size=2)
-#
-#     # Run using a model that works up to triangles.
-#     model = SIN0(num_input_features=1, num_classes=3, num_layers=3, hidden=5, max_dim=2,
-#                  jump_mode='max')
-#     # We use the model in eval mode to avoid problems with batch norm.
-#     model.eval()
-#
-#     preds1 = []
-#     for batch in data_loader:
-#         out = model.forward(batch)
-#         preds1.append(out)
-#     preds1 = torch.cat(preds1, dim=0)
-#
-#     # Run using a model that works up to edges.
-#     model = SIN0(num_input_features=1, num_classes=3, num_layers=3, hidden=5, max_dim=1,
-#                  jump_mode='max')
-#     model.eval()
-#
-#     data_loader = DataLoader(data_list, batch_size=2, max_dim=1)
-#     preds2 = []
-#     for batch in data_loader:
-#         out = model.forward(batch)
-#         preds2.append(out)
-#     preds2 = torch.cat(preds2, dim=0)
-#
-#     # Make sure the two outputs are different. The model using triangles set the triangle outputs
-#     # to zero, so the output of the readout should also be different.
-#     assert not torch.equal(preds1, preds2)
+def test_sin_model_with_batching_over_complexes_missing_triangles():
+    """Check this runs without errors"""
+    data_list = get_testing_complex_list()
+    data_loader = DataLoader(data_list, batch_size=2)
+
+    # Run using a model that works up to triangles.
+    model = SIN0(num_input_features=1, num_classes=3, num_layers=3, hidden=5, max_dim=2,
+                 jump_mode='max')
+    # We use the model in eval mode to avoid problems with batch norm.
+    model.eval()
+
+    preds1 = []
+    for batch in data_loader:
+        out = model.forward(batch)
+        preds1.append(out)
+    preds1 = torch.cat(preds1, dim=0)
+
+    # Run using a model that works up to edges.
+    model = SIN0(num_input_features=1, num_classes=3, num_layers=3, hidden=5, max_dim=1,
+                 jump_mode='max')
+    model.eval()
+
+    data_loader = DataLoader(data_list, batch_size=2, max_dim=1)
+    preds2 = []
+    for batch in data_loader:
+        out = model.forward(batch)
+        preds2.append(out)
+    preds2 = torch.cat(preds2, dim=0)
+
+    # Make sure the two outputs are different. The model using triangles set the triangle outputs
+    # to zero, so the output of the readout should also be different.
+    assert not torch.equal(preds1, preds2)
 
 
 
