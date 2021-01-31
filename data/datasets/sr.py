9ee010dacdb532208e5b9e84fb66461703129326
Noired <ffrasca@twitter.com>
2021-01-31 17:16:27 +0000

    Added small modifications for SR_datasets; we now allow for an arbitrary representation dimension

--- data/datasets/sr.py
+++ data/datasets/sr.py
@@ -6,20 +6,30 @@ from data.sr_utils import load_sr_dataset
 from data.utils import compute_clique_complex_with_gudhi
 from data.datasets import InMemoryComplexDataset
 
+import os.path as osp
+import errno
+
+def makedirs(path):
+    try:
+        os.makedirs(osp.expanduser(osp.normpath(path)))
+    except OSError as e:
+        if e.errno != errno.EEXIST and osp.isdir(path):
+            raise e
 
 class SRDataset(InMemoryComplexDataset):
 
     def __init__(self, root, name, max_dim=2, num_classes=2,
                  train_ids=None, val_ids=None, test_ids=None):
         self.name = name
+        self._num_classes = num_classes
         super(SRDataset, self).__init__(root, max_dim=max_dim, num_classes=num_classes)
         
+        with open(self.processed_paths[0], 'rb') as handle:
+            self._data_list = pickle.load(handle)
+            
         self.train_ids = list(range(self.len())) if train_ids is None else train_ids
         self.val_ids = list(range(self.len())) if val_ids is None else val_ids
         self.test_ids = list(range(self.len())) if test_ids is None else test_ids
-
-        with open(self.processed_paths[0], 'rb') as handle:
-            self._data_list = pickle.load(handle)
 
     @property
     def processed_file_names(self):
@@ -45,8 +55,10 @@ class SRDataset(InMemoryComplexDataset):
                 else:
                     assert num_features[dim] == complex.chains[dim].num_features
             complexes.append(complex)
-        self.max_dim = max_dim
-        y = torch.ones(len(complexes), dtype=torch.long)
+        if max_dim != self.max_dim:
+            self.max_dim = max_dim
+            makedirs(self.processed_dir)
+        y = torch.ones(self.num_classes, dtype=torch.long)
         for complex in complexes:
             complex.y = y
         path = self.processed_paths[0]
