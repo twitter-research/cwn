591da08f42f78f7c027d6bc2874584e026da1309
Cristian Bodnar <crisbodnar96@gmail.com>
2021-02-08 21:01:23 +0000

    Fix gitignore

--- data/datasets/__init__.py
+++
@@ -1,4 +0,0 @@
-from data.datasets.dataset import ComplexDataset, InMemoryComplexDataset
-from data.datasets.sr import SRDataset, load_sr_graph_dataset
-from data.datasets.cluster import ClusterDataset
-from data.datasets.tu import TUDataset
