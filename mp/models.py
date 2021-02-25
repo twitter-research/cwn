a7f5636807aff1ae44f76a992fcd804d530de978
Cristian Bodnar <crisbodnar96@gmail.com>
2021-02-25 15:12:17 +0000

    Add BN

--- mp/models.py
+++ mp/models.py
@@ -501,9 +501,9 @@ class EdgeOrient(torch.nn.Module):
         self.pooling_fn = get_pooling_fn(readout)
         for i in range(num_layers):
             layer_dim = num_input_features if i == 0 else hidden
-            update_up = Sequential(Linear(layer_dim, hidden))
-            update_down = Sequential(Linear(layer_dim, hidden))
-            update = Sequential(Linear(layer_dim, hidden))
+            update_up = Sequential(Linear(layer_dim, hidden), BN(hidden))
+            update_down = Sequential(Linear(layer_dim, hidden), BN(hidden))
+            update = Sequential(Linear(layer_dim, hidden), BN(hidden))
             self.convs.append(
                 OrientedConv(dim=1, up_msg_size=layer_dim, down_msg_size=layer_dim,
                     update_up_nn=update_up, update_down_nn=update_down, update_nn=update,
