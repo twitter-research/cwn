7843b59fa12d922cbbff9aea78cfc4164285ad65
Noired <ffrasca@twitter.com>
2021-03-17 19:27:10 +0000

    Added latest minor modifications for RDTM tuning

--- exp/launch_tu_tuning.sh
+++ exp/launch_tu_tuning.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-for i in $( seq 0 6 )
+for i in $( seq 0 5 )
 do
     # python3 -m exp.run_tu_tuning --conf ~/git/scn/exp/tuning_configurations/RDTB_grid.yml --code redditbinary_20210219 --idx $i &
-    python3 -m exp.run_tu_tuning --conf ~/git/scn/exp/tuning_configurations/RDTB_grid.yml --code redditbinary_20210223 --idx $i &
+    # python3 -m exp.run_tu_tuning --conf ~/git/scn/exp/tuning_configurations/RDTB_grid.yml --code redditbinary_20210223 --idx $i &
+    python3 -m exp.run_tu_tuning --conf ~/git/scn/exp/tuning_configurations/RDTM_grid.yml --code redditmulti_20210227 --idx $i &
 done
