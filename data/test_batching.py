e1879b24662b504a389effbfe49f74975973e4d7
Noired <ffrasca@twitter.com>
2021-05-05 09:59:23 +0000

    Modified test for face_attrs, which now passes

--- data/test_batching.py
+++ data/test_batching.py
@@ -891,6 +891,53 @@ def test_batching_returns_the_same_face_attr_on_proteins():
 # index-select operation happens later. The second of these tests fail due to
 # this discrepancy.
             
+# def test_batching_returns_the_same_face_attr():
+#     data_list = get_testing_complex_list()
+
+#     # Try multiple parameters
+#     dims = [1, 2, 3]
+#     bs = list(range(2, 11))
+#     params = itertools.product(bs, dims)
+#     for batch_size, batch_max_dim, in params:
+#         data_loader = DataLoader(data_list, batch_size=batch_size, max_dim=batch_max_dim)
+
+#         batched_x = [[] for _ in range(batch_max_dim+1)]
+#         for batch in data_loader:
+#             params = batch.get_all_chain_params()
+#             assert len(params) <= batch_max_dim+1
+#             for dim in range(len(params)):
+#                 if params[dim].kwargs['face_attr'] is not None:
+#                     batched_x[dim].append(params[dim].kwargs['face_attr'])
+
+#         batched_xs = [None for _ in range(batch_max_dim+1)]
+#         for i in range(batch_max_dim+1):
+#             if len(batched_x[i]) > 0:
+#                 batched_xs[i] = torch.cat(batched_x[i], dim=0)
+
+#         # Un-batched
+#         x = [[] for _ in range(batch_max_dim+1)]
+#         for complex in data_list:
+#             params = complex.get_all_chain_params()
+#             for dim in range(min(len(params), batch_max_dim+1)):
+#                 if params[dim].kwargs['face_attr'] is not None:
+#                     x[dim].append(params[dim].kwargs['face_attr'])
+
+#         xs = [None for _ in range(batch_max_dim+1)]
+#         for i in range(batch_max_dim+1):
+#             if len(x[i]) > 0:
+#                 xs[i] = torch.cat(x[i], dim=0)
+
+#         for i in range(batch_max_dim+1):
+#             if xs[i] is None or batched_xs[i] is None:
+#                 assert xs[i] == batched_xs[i]
+#             else:
+#                 # import pdb; pdb.set_trace()
+#                 assert len(xs[i]) == len(batched_xs[i])
+#                 assert torch.equal(xs[i], batched_xs[i])
+
+# ---------------------------------------------------------------------
+
+
 def test_batching_returns_the_same_face_attr():
     data_list = get_testing_complex_list()
 
@@ -907,7 +954,10 @@ def test_batching_returns_the_same_face_attr():
             assert len(params) <= batch_max_dim+1
             for dim in range(len(params)):
                 if params[dim].kwargs['face_attr'] is not None:
-                    batched_x[dim].append(params[dim].kwargs['face_attr'])
+                    assert params[dim].face_index is not None
+                    face_attrs = params[dim].kwargs['face_attr']
+                    batched_x[dim].append(
+                        torch.index_select(face_attrs, 0, params[dim].face_index[0]))
 
         batched_xs = [None for _ in range(batch_max_dim+1)]
         for i in range(batch_max_dim+1):
@@ -920,7 +970,11 @@ def test_batching_returns_the_same_face_attr():
             params = complex.get_all_chain_params()
             for dim in range(min(len(params), batch_max_dim+1)):
                 if params[dim].kwargs['face_attr'] is not None:
-                    x[dim].append(params[dim].kwargs['face_attr'])
+                    
+                    assert params[dim].face_index is not None
+                    face_attrs = params[dim].kwargs['face_attr']
+                    x[dim].append(
+                        torch.index_select(face_attrs, 0, params[dim].face_index[0]))
 
         xs = [None for _ in range(batch_max_dim+1)]
         for i in range(batch_max_dim+1):
@@ -934,8 +988,7 @@ def test_batching_returns_the_same_face_attr():
                 # import pdb; pdb.set_trace()
                 assert len(xs[i]) == len(batched_xs[i])
                 assert torch.equal(xs[i], batched_xs[i])
-
-# ---------------------------------------------------------------------
+                
 
 def test_data_loader_shuffling():
     dataset = load_dataset('PROTEINS', max_dim=3, fold=0, init_method='mean')
