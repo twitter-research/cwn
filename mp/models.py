f6e85a6b413b735a4f9174e78622c260d697b190
Cristian Bodnar <crisbodnar96@gmail.com>
2021-05-07 14:16:08 +0100

    Fix nit

--- mp/models.py
+++ mp/models.py
@@ -204,8 +204,7 @@ class SparseSIN(torch.nn.Module):
             conv.reset_parameters()
         if self.jump_mode is not None:
             self.jump.reset_parameters()
-        self.lin1.reset_parameters()
-        self.lin2.reset_parameters()
+        self.lin1s.reset_parameters()
 
     def pool_complex(self, xs, data):
         # All complexes have nodes so we can extract the batch size from chains[0]
