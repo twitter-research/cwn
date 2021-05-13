696cab72ff7c7f7206cc46418522ec92d7c65940
Cristian Bodnar <crisbodnar96@gmail.com>
2021-05-13 08:29:16 +0100

    Fix edge feature bug

--- exp/run_mol.sh
+++ exp/run_mol.sh
@@ -6,3 +6,9 @@ python -m exp.run_exp --dataset ZINC --train_eval_period 10 --epochs 300 --batch
   --init_method sum --jump_mode cat --lr 0.003 --model zinc_sparse_sin --nonlinearity relu \
   --num_layers 3 --readout mean --max_ring_size=6 --task_type=regression --eval_metric=mae \
   --minimize --lr_scheduler='ReduceLROnPlateau'
+
+CUDA_VISIBLE_DEVICES=3 python -m exp.run_exp --dataset ZINC --train_eval_period 10 \
+  --epochs 300 --batch_size 128   --drop_rate 0.0 --drop_position lin2 --emb_dim 128 \
+  --max_dim 2 --final_readout sum --init_method sum --lr 0.001 --model zinc_sparse_sin \
+  --nonlinearity relu --num_layers 4 --readout sum --max_ring_size=10 --task_type=regression \
+  --eval_metric=mae --minimize --lr_scheduler='ReduceLROnPlateau' --use_cofaces=True
