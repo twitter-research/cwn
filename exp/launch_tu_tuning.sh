1ebd6c9a7ea470938cce3d0ed08a3c3bd0c4c234
Noired <ffrasca@twitter.com>
2021-05-15 12:50:56 +0000

    Fixed launcher

--- exp/launch_tu_tuning.sh
+++ exp/launch_tu_tuning.sh
@@ -3,7 +3,7 @@ low=0
 high=7
 gridpath="<path_to_grid>"
 expname="<exp_name>"
-python3 -m exp.build_dataset
+python3 -m exp.prepare_tu_tuning $gridpath
 for i in $( seq $low $high )
 do
     python3 -m exp.run_tu_tuning --conf $gridpath --code $expname --idx $i &
