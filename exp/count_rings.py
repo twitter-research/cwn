7eb691ab1959644c73cffbb8261f6733e00fe70f
Cristian Bodnar <crisbodnar96@gmail.com>
2021-05-20 17:20:25 +0100

    Fix other comments

--- exp/count_rings.py
+++ exp/count_rings.py
@@ -6,18 +6,18 @@ from tqdm import tqdm
 from data.data_loading import load_graph_dataset
 from data.utils import get_rings
 
-parser = argparse.ArgumentParser(description='SCN experiment.')
+parser = argparse.ArgumentParser(description='Ring counting experiment.')
 parser.add_argument('--dataset', type=str, default="ZINC",
                     help='dataset name (default: ZINC)')
 parser.add_argument('--max_ring_size', type=int, default=12,
                     help='maximum ring size to look for')
 
 
-def get_ring_stats(dataset, max_ring):
-    keys = list(range(3, max_ring))
+def get_ring_counts(dataset, max_ring):
+    keys = list(range(3, max_ring+1))
     ring_cards = {key: list() for key in keys}
     for graph in tqdm(dataset):
-        rings = get_rings(graph.edge_index, max_k=max_ring - 1)
+        rings = get_rings(graph.edge_index, max_k=max_ring)
         rings_per_graph = {key: 0 for key in keys}
         for ring in rings:
             k = len(ring)
@@ -27,7 +27,7 @@ def get_ring_stats(dataset, max_ring):
     return ring_cards
 
 
-def combine_all_stats(*stats):
+def combine_all_counts(*stats):
     all_stats = dict()
 
     for k in stats[0].keys():
@@ -49,7 +49,7 @@ def print_stats(stats):
         nz = np.count_nonzero(stats[k])
         print(
             f'Ring {k:02d} => Min: {min:.3f}, Max: {max:.3f}, Mean:{mean:.3f}, Median: {med:.3f}, '
-            f'Sum: {sum}, Non-zero: {nz}')
+            f'Sum: {sum:05d}, Non-zero: {nz:05d}')
 
 
 def exp_main(passed_args):
@@ -66,17 +66,17 @@ def exp_main(passed_args):
         test = [graph_list[i] for i in test_ids]
 
     print("Counting rings on the training set ....")
-    train_stats = get_ring_stats(train, args.max_ring_size)
+    train_stats = get_ring_counts(train, args.max_ring_size)
     print("Counting rings on the validation set ....")
-    val_stats = get_ring_stats(val, args.max_ring_size)
+    val_stats = get_ring_counts(val, args.max_ring_size)
 
     test_stats = None
     if test is not None:
         print("Counting rings on the test set ....")
-        test_stats = get_ring_stats(test, args.max_ring_size)
-        all_stats = combine_all_stats(train_stats, val_stats, test_stats)
+        test_stats = get_ring_counts(test, args.max_ring_size)
+        all_stats = combine_all_counts(train_stats, val_stats, test_stats)
     else:
-        all_stats = combine_all_stats(train_stats, val_stats)
+        all_stats = combine_all_counts(train_stats, val_stats)
 
     print("=============== Train ================")
     print_stats(train_stats)
