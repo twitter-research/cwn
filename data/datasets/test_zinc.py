ffebac93f9ec94a588bb6177d6ee496342017f10
Cristian Bodnar <crisbodnar96@gmail.com>
2021-05-21 11:04:45 +0100

    Add option for heavy test

--- data/datasets/test_zinc.py
+++ data/datasets/test_zinc.py
@@ -1,24 +1,31 @@
-from data.data_loading import load_dataset
+from data.data_loading import load_dataset, load_graph_dataset
+import torch
+import os.path as osp
+import pytest
+from torch_geometric.datasets import ZINC
 
-# We keep this test commented because it takes a lot of compute but it is useful to run manually
-# from time to time.
 
-# def test_zinc_actually_works():
-#     dataset1 = load_dataset("ZINC", max_ring_size=7, use_edge_features=True)
-#     dataset2 = load_dataset("ZINC", max_ring_size=12, use_edge_features=True)
-#
-#     expected = 33242
-#     total1, total2 = 0, 0
-#     for i, _ in enumerate(dataset1):
-#         data1, data2 = dataset1[i], dataset2[i]
-#         assert data1.chains[0].num_simplices == data2.chains[0].num_simplices
-#         assert data1.chains[1].num_simplices == data2.chains[1].num_simplices
-#
-#         if len(data1.chains) > 2:
-#             total1 += data1.chains[2].num_simplices
-#         if len(data2.chains) > 2:
-#             total2 += data2.chains[2].num_simplices
-#         # assert data1.chains[2].num_simplices == data2.chains[2].num_simplices
-#         # assert data1.chains[2].face_index.max() == data2.chains[2].face_index.max()
-#     print(total1, total2)
+@pytest.mark.slow
+def test_zinc_splits_are_retained():
+    dataset1 = load_dataset("ZINC", max_ring_size=3, use_edge_features=True)
+    dataset1_train = dataset1.get_split('train')
+    dataset1_valid = dataset1.get_split('valid')
+    dataset1_test = dataset1.get_split('test')
 
+    raw_dir = osp.join(dataset1.root, 'raw')
+    dataset2_train = ZINC(raw_dir, subset=True, split='train')
+    dataset2_valid = ZINC(raw_dir, subset=True, split='val')
+    dataset2_test = ZINC(raw_dir, subset=True, split='test')
+
+    datasets = [
+        (dataset1_train, dataset2_train),
+        (dataset1_valid, dataset2_valid),
+        (dataset1_test, dataset2_test),
+    ]
+
+    for datas1, datas2 in datasets:
+        for i, _ in enumerate(datas1):
+            data1, data2 = datas1[i], datas2[i]
+            assert torch.equal(data1.chains[0].x, data2.x)
+            assert torch.equal(data1.y, data2.y)
+            assert data1.chains[1].x.size(0) == (data2.edge_index.size(1) // 2)
