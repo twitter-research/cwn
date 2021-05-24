Fixes to make SR exp works with new updates and parallel computation

data/datasets/sr.py
@@ -4,6 +4,7 @@ import pickle
 
 from data.sr_utils import load_sr_dataset
 from data.utils import compute_clique_complex_with_gudhi, compute_ring_2complex
+from data.utils import convert_graph_dataset_with_rings, convert_graph_dataset_with_gudhi
 from data.datasets import InMemoryComplexDataset
 from definitions import ROOT_DIR
 from torch_geometric.data import Data
@@ -18,16 +19,15 @@ def makedirs(path):
         if e.errno != errno.EEXIST and osp.isdir(path):
             raise e
 
-def load_sr_graph_dataset(name, root=os.path.join(ROOT_DIR, 'datasets'), emb_dim=16):
+def load_sr_graph_dataset(name, root=os.path.join(ROOT_DIR, 'datasets')):
     raw_dir = os.path.join(root, 'SR_graphs', 'raw')
     load_from = os.path.join(raw_dir, '{}.g6'.format(name))
     data = load_sr_dataset(load_from)
-    y = torch.ones(emb_dim, dtype=torch.long)
     graphs = list()
     for datum in data:
         edge_index, num_nodes = datum
         x = torch.ones(num_nodes, 1, dtype=torch.float32)
-        graph = Data(x=x, edge_index=edge_index, y=y, num_nodes=num_nodes)
+        graph = Data(x=x, edge_index=edge_index, y=None, edge_attr=None, num_nodes=num_nodes)
         graphs.append(graph) 
     train_ids = list(range(len(graphs)))
     val_ids = list(range(len(graphs)))
@@ -37,9 +37,10 @@ def load_sr_graph_dataset(name, root=os.path.join(ROOT_DIR, 'datasets'), emb_dim
 class SRDataset(InMemoryComplexDataset):
 
     def __init__(self, root, name, max_dim=2, num_classes=2,
-                 train_ids=None, val_ids=None, test_ids=None, include_down_adj=False, max_ring_size=None):
+                 train_ids=None, val_ids=None, test_ids=None, include_down_adj=False, max_ring_size=None, n_jobs=2):
         self.name = name
         self._num_classes = num_classes
+        self._n_jobs = n_jobs
         assert max_ring_size is None or max_ring_size > 3
         self._max_ring_size = max_ring_size
         cellular = (max_ring_size is not None)
@@ -67,25 +68,30 @@ class SRDataset(InMemoryComplexDataset):
         return ['{}_complex_list.pt'.format(self.name)]       
 
     def process(self):
-        data = load_sr_dataset(os.path.join(self.raw_dir, self.name + '.g6'))
+        
+        graphs, _, _, _ = load_sr_graph_dataset(self.name)
         exp_dim = self.max_dim
-
+        if self._cellular:
+            print(f"Converting the {self.name} dataset to a cell complex...")
+            complexes, _, _ = convert_graph_dataset_with_rings(
+                graphs,
+                max_ring_size=self._max_ring_size,
+                include_down_adj=self.include_down_adj,
+                init_method='sum',
+                init_edges=True,
+                init_rings=True,
+                n_jobs=self._n_jobs)
+        else:
+            print(f"Converting the {self.name} dataset with gudhi...")
+            complexes, _, _ = convert_graph_dataset_with_gudhi(
+                graphs,
+                expansion_dim=exp_dim,                                               
+                include_down_adj=self.include_down_adj,                    
+                init_method='sum')
+        
         num_features = [None for _ in range(exp_dim+1)]
-        complexes = list()
         max_dim = -1
-        for datum in data:
-            edge_index, num_nodes = datum
-            x = torch.ones(num_nodes, 1, dtype=torch.float32)
-            
-            if self._cellular:
-                complex = compute_ring_2complex(x, edge_index, None,
-                          num_nodes, max_k=self._max_ring_size,
-                          include_down_adj=self.include_down_adj,
-                          init_edges=True, init_rings=True)
-            else:
-                complex = compute_clique_complex_with_gudhi(x, edge_index, num_nodes,
-                    expansion_dim=exp_dim, include_down_adj=self.include_down_adj)
-                
+        for complex in complexes:
             if complex.dimension > max_dim:
                 max_dim = complex.dimension
             if self._max_ring_size is not None:
@@ -95,10 +101,42 @@ class SRDataset(InMemoryComplexDataset):
                     num_features[dim] = complex.chains[dim].num_features
                 else:
                     assert num_features[dim] == complex.chains[dim].num_features
-            complexes.append(complex)
         if max_dim != self.max_dim:
             self.max_dim = max_dim
             makedirs(self.processed_dir)
+        
+#         data = load_sr_dataset(os.path.join(self.raw_dir, self.name + '.g6'))
+#         exp_dim = self.max_dim
+
+#         num_features = [None for _ in range(exp_dim+1)]
+#         complexes = list()
+#         max_dim = -1
+#         for datum in data:
+#             edge_index, num_nodes = datum
+#             x = torch.ones(num_nodes, 1, dtype=torch.float32)
+            
+#             if self._cellular:
+#                 complex = compute_ring_2complex(x, edge_index, None,
+#                           num_nodes, max_k=self._max_ring_size,
+#                           include_down_adj=self.include_down_adj,
+#                           init_edges=True, init_rings=True)
+#             else:
+#                 complex = compute_clique_complex_with_gudhi(x, edge_index, num_nodes,
+#                     expansion_dim=exp_dim, include_down_adj=self.include_down_adj)
+                
+#             if complex.dimension > max_dim:
+#                 max_dim = complex.dimension
+#             if self._max_ring_size is not None:
+#                 assert max_dim <= 2
+#             for dim in range(complex.dimension + 1):
+#                 if num_features[dim] is None:
+#                     num_features[dim] = complex.chains[dim].num_features
+#                 else:
+#                     assert num_features[dim] == complex.chains[dim].num_features
+#             complexes.append(complex)
+#         if max_dim != self.max_dim:
+#             self.max_dim = max_dim
+#             makedirs(self.processed_dir)
             
         # Now we save in opt format.
         path = self.processed_paths[0]

exp/train_utils.py
@@ -114,20 +114,17 @@ def eval(model, device, loader, evaluator, task_type, debug_dataset=None):
         with torch.no_grad():
             pred = model(batch)
             
-            if isinstance(loss_fn, torch.nn.CrossEntropyLoss):
-                targets = batch.y.view(-1,)
-            else:
-                targets = batch.y.to(torch.float32).view(pred.shape)
-            mask = ~torch.isnan(targets)  # In some ogbg-mol* datasets we may have null targets.
-            
             if task_type != 'isomorphism':
+                if isinstance(loss_fn, torch.nn.CrossEntropyLoss):
+                    targets = batch.y.view(-1,)
+                else:
+                    targets = batch.y.to(torch.float32).view(pred.shape)
+                mask = ~torch.isnan(targets)  # In some ogbg-mol* datasets we may have null targets.
                 loss = loss_fn(pred[mask], targets[mask])
+                losses.append(loss.detach().cpu().item())
             else:
                 assert loss_fn is None
-                
-        if loss_fn is not None:
-            losses.append(loss.detach().cpu().item())
-            
+
         if task_type != 'isomorphism':
             targets = batch.y
             if not isinstance(loss_fn, torch.nn.CrossEntropyLoss):