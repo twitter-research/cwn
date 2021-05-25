6c24b493192a87d7a11816cf7c1540da858872f8
Cristian Bodnar <crisbodnar96@gmail.com>
2021-05-25 13:40:30 +0100

    Make all feat const

--- data/datasets/ringtree_utils.py
+++ data/datasets/ringtree_utils.py
@@ -53,9 +53,9 @@ def generate_ringtree_graph(nodes, target_label):
 
     # Initialise the feature matrix. All nodes have random features except the target and the opp.
     # The target has [0, ..., 0] while the opposite has the one-hot encoded target label
-    x = np.random.standard_normal(size=(nodes, len(target_label)))
+    # x = np.random.standard_normal(size=(nodes, len(target_label)))
     # x = np.zeros((nodes, len(target_label)))
-    # x = np.ones((nodes, len(target_label)))
+    x = np.ones((nodes, len(target_label)))
 
     x[opposite_node, :] = target_label
     x = torch.tensor(x, dtype=torch.float32)
