27549bf6cf4b9c5687055073afd10ebc7fdcb1a4
Noired <ffrasca@twitter.com>
2021-05-25 12:49:04 +0000

    Added multiple seeds experimentation

--- exp/run_sr_exp.py
+++ exp/run_sr_exp.py
@@ -12,15 +12,15 @@ from exp.run_exp import main
 #--jump_mode None
 
 __families__ = [
-    'sr16622',
-    'sr251256',
+    # 'sr16622',
+    # 'sr251256',
     'sr261034',
-    'sr281264',
-    'sr291467',
+    # 'sr281264',
+    # 'sr291467',
     # 'sr351668',
-    'sr351899',
-    'sr361446',
-    'sr401224'
+    # 'sr351899',
+    # 'sr361446',
+    # 'sr401224'
 ]
 
 __max_dim__ = [
@@ -37,35 +37,50 @@ __max_dim__ = [
 if __name__ == "__main__":
     
     # standard args
-    parser = get_parser()
     passed_args = sys.argv[1:]
-    assert 'dataset' not in passed_args
-    assert 'exp_name' not in passed_args
+    assert '--seed' not in passed_args
+    assert '--dataset' not in passed_args
+    assert '--exp_name' not in passed_args
+    parser = get_parser()
+    args = parser.parse_args(copy.copy(passed_args))
     ts = str(time.time())
-    passed_args += ['--result_folder', os.path.join(ROOT_DIR, 'exp', 'results', 'sr-{}'.format(ts))]
+    result_folder = os.path.join(ROOT_DIR, 'exp', 'results', 'sr-{}'.format(ts))
+    passed_args += ['--result_folder', result_folder]
     
     # run each experiment separately and gather results
-    results = list()
+    results = [list() for _ in __families__]
     for f, family in enumerate(__families__):
-        current_args = copy.copy(passed_args) + ['--dataset', family, '--exp_name', family]
-        if '--max_dim' not in passed_args:
-            if '--max_ring_size' not in passed_args:
-                current_args += ['--max_dim', str(__max_dim__[f])]
+        for seed in range(args.start_seed, args.stop_seed + 1):
+            print(f'[i] family {family}, seed {seed}')
+            current_args = copy.copy(passed_args) + ['--dataset', family, '--exp_name', family, '--seed', str(seed)]
+            if '--max_dim' not in passed_args:
+                if '--max_ring_size' not in passed_args:
+                    current_args += ['--max_dim', str(__max_dim__[f])]
+                else:
+                    current_args += ['--max_dim', str(2)]
             else:
-                current_args += ['--max_dim', str(2)]
-        else:
-            assert '--max_ring_size' not in passed_args
-        parsed_args = parser.parse_args(current_args)
-        curves = main(parsed_args)
-        results.append(curves)
+                assert '--max_ring_size' not in passed_args
+            parsed_args = parser.parse_args(current_args)
+            curves = main(parsed_args)
+            results[f].append(curves)
             
     msg = ''
     for f, family in enumerate(__families__):
         curves = results[f]
-        test_perf = curves['last_test']
+        test_perfs = [curve['last_test'] for curve in curves]
+        assert len(test_perfs) == args.stop_seed + 1 - args.start_seed
+        mean = np.mean(test_perfs)
+        std_err = np.std(test_perfs) / float(len(test_perfs))
         msg += (
-            f'Dataset:        {family}\n'
-            f'Failure rate:   {test_perf}\n'
+            f'Dataset:               {family}\n'
+            f'Mean failure rate:     {mean}\n'
+            f'StdErr failure rate:   {std_err}\n'
             '-----------------------------------------------\n')
     print(msg)
-
+    
+    # additionally write msg and configuration on file
+    msg += str(args)
+    filename = os.path.join(result_folder, 'result.txt')
+    print('Writing results at: {}'.format(filename))
+    with open(filename, 'w') as handle:
+        handle.write(msg)
