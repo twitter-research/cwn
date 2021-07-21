55cf6958a7e664a35aafb46ea17b2ab755362645
Cristian Bodnar <crisbodnar96@gmail.com>
2021-07-21 11:54:49 +0300

    Improve script for ring exp

--- exp/run_ring_exp.py
+++ exp/run_ring_exp.py
@@ -1,11 +1,13 @@
+import os
 import sys
 import copy
 import subprocess
+import numpy as np
 
 from exp.parser import get_parser
 from exp.run_exp import main
 
-RING_SIZE = list(range(10, 32, 2))
+RING_SIZES = list(range(10, 32, 2))
 
 
 def exp_main(passed_args):
@@ -15,35 +17,59 @@ def exp_main(passed_args):
     parser = get_parser()
     args = parser.parse_args(copy.copy(passed_args))
     assert args.max_ring_size is None
-    assert args.num_layers is None
 
     # run each experiment separately and gather results
-    results = list()
-    # We use the ring_size as a "fold" for the dataset. This is just a trick to save the results.
-    for fold in range(len(RING_SIZE)):
-        max_ring_size = RING_SIZE[fold]
-        num_layers = 3 if args.model == 'ring_sparse_sin' else max_ring_size // 2
-        current_args = (copy.copy(passed_args) + ['--fold', str(fold)] +
-                        ['--max_ring_size', str(max_ring_size)] +
-                        ['--num_layers', str(num_layers)])
-        parsed_args = parser.parse_args(current_args)
-        curves = main(parsed_args)
-        results.append(curves)
+    train_results = {seed: [] for seed in range(args.start_seed, args.stop_seed + 1)}
+    val_results = {seed: [] for seed in range(args.start_seed, args.stop_seed + 1)}
+    for seed in range(args.start_seed, args.stop_seed + 1):
+        # We use the ring_size as a "fold" for the dataset.
+        # This is just a hack to save the results properly using our usual infrastructure.
+        for fold in range(len(RING_SIZES)):
+            max_ring_size = RING_SIZES[fold]
+            num_layers = 3 if args.model == 'ring_sparse_sin' else max_ring_size // 2
+            current_args = (copy.copy(passed_args) + ['--fold', str(fold)] +
+                            ['--max_ring_size', str(max_ring_size)] +
+                            ['--num_layers', str(num_layers)] +
+                            ['--seed', str(seed)])
+            parsed_args = parser.parse_args(current_args)
+            # Check that the default parameter value (5) was overwritten
+            assert parsed_args.num_layers == num_layers
+            curves = main(parsed_args)
 
-    # Extract results
-    last_train = [curves['last_train'] for curves in results]
-    last_val = [curves['last_val'] for curves in results]
+            # Extract results
+            train_results[seed].append(curves['last_train'])
+            val_results[seed].append(curves['last_val'])
 
-    print(sha)
+    msg = (
+        f"========= Final result ==========\n"
+        f'Dataset:                {args.dataset}\n'
+        f'SHA:                    {sha}\n'
+        f'----------- Train ----------\n')
 
-    print("Train")
-    print(last_train)
+    for ring_size, results in train_results.items():
+        mean = np.mean(results)
+        std = np.std(results)
+        msg += f'Ring size: {ring_size}            {mean}+-{std}\n'
 
-    print("Val")
-    print(last_val)
+    msg += f'----------- Test ----------\n'
+
+    for ring_size, results in val_results.items():
+        mean = np.mean(results)
+        std = np.std(results)
+        msg += f'Ring size: {ring_size}            {mean}+-{std}\n'
+
+    print(msg)
+
+    # additionally write msg and configuration on file
+    msg += str(args)
+    filename = os.path.join(args.result_folder, f'{args.dataset}-{args.exp_name}/result.txt')
+    print('Writing results at: {}'.format(filename))
+    with open(filename, 'w') as handle:
+        handle.write(msg)
 
 
 if __name__ == "__main__":
     passed_args = sys.argv[1:]
     assert '--fold' not in passed_args
+    assert '--seed' not in passed_args
     exp_main(passed_args)
