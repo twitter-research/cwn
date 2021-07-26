fc98db78dd80d4df935a6bbe284cd7934af40be1
Cristian Bodnar <crisbodnar96@gmail.com>
2021-07-26 18:18:48 +0300

    Update data/datasets/tu.py
    Co-authored-by: Noired <Noired@users.noreply.github.com>

--- data/datasets/tu.py
+++ data/datasets/tu.py
@@ -35,7 +35,7 @@ def load_tu_graph_dataset(name, root=os.path.join(ROOT_DIR, 'datasets'), degree_
 
 
 class TUDataset(InMemoryComplexDataset):
-    """A class for loading the TUDatasets in cell complex format."""
+    """A dataset of complexes obtained by lifting graphs from TUDatasets."""
 
     def __init__(self, root, name, max_dim=2, num_classes=2, degree_as_tag=False, fold=0,
                  init_method='sum', seed=0, include_down_adj=False, max_ring_size=None):
@@ -127,4 +127,3 @@ class TUDataset(InMemoryComplexDataset):
         #     'valid': self.tune_val_ids,
         #     'test': self.tune_test_ids}
         # return idx_split
-
