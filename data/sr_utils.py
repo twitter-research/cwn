cb33d083a7516d42f9af8b3176ace45820a16ed7
Cristian Bodnar <crisbodnar96@gmail.com>
2021-07-26 18:20:02 +0300

    Update data/sr_utils.py
    Co-authored-by: Noired <Noired@users.noreply.github.com>

--- data/sr_utils.py
+++ data/sr_utils.py
@@ -4,7 +4,7 @@ from torch_geometric.utils import to_undirected
 
 
 def load_sr_dataset(path):
-    """Load the Strongly Regular Graph Dataset from the supplied path.s"""
+    """Load the Strongly Regular Graph Dataset from the supplied path."""
     nx_graphs = nx.read_graph6(path)
     graphs = list()
     for nx_graph in nx_graphs:
